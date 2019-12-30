ab9f91fccde898756dc2057c27a58c219d733a48
Vincent Chi <song374561@chivincent.net>
2019-12-30 13:53:59 +0800

    feat: create demo code into main function.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+use std::env;
+use std::process;
+use std::fs::File;
+use std::io::Read;
 use ::graphic_info::GraphicInfo;
 
-fn main() {
-    let bytes = [0; 40];
-    let graphic_info = GraphicInfo::new(&bytes);
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", graphic_info);
+    if args.len() != 2 {
+        eprintln!("Usage: ./graphic-info [GraphicInfo.bin]");
+        process::exit(1);
+    }
+
+    let mut file = File::open(&args[1])?;
+    let mut buf = [0; 40];
+    let mut collection: Vec<GraphicInfo> = vec!();
+
+    loop {
+        match file.read_exact(&mut buf) {
+            Ok(_) => collection.push(GraphicInfo::new(&buf).unwrap()),
+            Err(_) => break,
+        }
+    }
+
+    println!("{}", collection.len());
+
+    Ok(())
 }
