d772a69264be2a79467d65413580db243fbf7cd5
Vincent Chi <song374561@chivincent.net>
2019-12-30 16:01:40 +0800

    feat: create GraphicInfoFile structure and implement Iterator trait.

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,39 @@ fn main() -> std::io::Result<()> {
                             .required(true))
                     .get_matches();
 
-    let mut file = File::open(matches.value_of("GraphicInfo.bin").unwrap())?;
-    let mut buf = [0; 40];
-    let mut collection: Vec<GraphicInfo> = vec!();
-
-    loop {
-        match file.read_exact(&mut buf) {
-            Ok(_) => collection.push(GraphicInfo::new(&buf).unwrap()),
-            Err(_) => break,
-        }
-    }
+    let file = GraphicInfoFile::new(matches.value_of("GraphicInfo.bin").unwrap())?;
+    let collection: Vec<GraphicInfo> = file.collect();
 
     println!("{}", collection.len());
 
     Ok(())
+}
+
+struct GraphicInfoFile {
+    file: File,
+}
+
+impl GraphicInfoFile {
+    fn new (path: &str) -> Result<Self, std::io::Error> {
+        let file = File::open(&path)?;
+
+        if file.metadata()?.len() % 40 != 0 {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Invalid input file size."));
+        }
+
+        Ok(Self{file})
+    }
+}
+
+impl Iterator for GraphicInfoFile {
+    type Item = GraphicInfo;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buf = [0; 40];
+
+        match self.file.read_exact(&mut buf) {
+            Ok(_) => return Some(GraphicInfo::new(&buf).unwrap()),
+            Err(_) => return None,
+        }
+    }
 }
