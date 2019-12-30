22b68a322e18766f89669d8f391a019956c6d55f
Vincent Chi <song374561@chivincent.net>
2019-12-30 15:34:50 +0800

    feat: get package name, version and authors from CARGO_PKG_* environment variables.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use ::graphic_info::GraphicInfo;
 use clap::{Arg, App};
 
 fn main() -> std::io::Result<()> {
-    let matches = App::new("graphic-info")
-                    .version("0.1")
-                    .author("Vincent Chi <song374561@chivincent.net>")
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+                    .version(env!("CARGO_PKG_VERSION"))
+                    .author(env!("CARGO_PKG_AUTHORS"))
                     .arg(Arg::with_name("GraphicInfo.bin")
                             .help("The path of GraphicInfo.bin")
                             .required(true))
