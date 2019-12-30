51cefa36bc841c4d3de96ce6969855a51641d90a
Vincent Chi <song374561@chivincent.net>
2019-12-30 10:50:42 +0800

    feat: create GraphicInfo structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct GraphicInfo {
+    id: u32,
+    address: u32,
+    length: u32,
+    offset_x: i32,
+    offset_y: i32,
+    width: u32,
+    height: u32,
+    tile_east: u8,
+    tile_south: u8,
+    unknown: [u8; 5],
+    map: u32,
+}
