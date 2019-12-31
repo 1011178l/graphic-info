63564e333346b47c00948bb90c508e747ea9149f
Vincent Chi <song374561@chivincent.net>
2019-12-31 18:37:18 +0800

    refactor: fix path string when creating file.

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,7 @@
 extern crate sqlite;
 
 use std::path::Path;
-use super::GraphicInfoFile;
-use sqlite::{Connection, Value};
+use sqlite::Connection;
 
 pub struct Database {
     pub connection: Connection,
@@ -12,7 +11,7 @@ impl Database {
     pub fn new(path: &str) -> Result<Self, std::io::Error> {
         let path = Path::new(path);
         if !path.exists() {
-            std::fs::File::create(path.to_str().unwrap())?;
+            std::fs::File::create(path)?;
         }
 
         let connection = sqlite::Connection::open(path).unwrap();
@@ -44,36 +43,5 @@ impl Database {
             );")?;
 
             Ok(())
-    }
-
-    pub fn insert(&self, graphic_info_file: GraphicInfoFile) {
-        let mut statement = self.connection.prepare(
-            "INSERT INTO graphic_info (
-                graphic_id, address, length, offset_x, offset_y, width, height, tile_east, tile_south, access, unknown0, unknown1, unknown2, unknown3, unknown4, map
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
-            .unwrap()
-            .cursor();
-        
-            for gi in graphic_info_file {
-                statement.bind(&[
-                    Value::Integer(gi.id as i64), 
-                    Value::Integer(gi.address as i64), 
-                    Value::Integer(gi.length as i64),
-                    Value::Integer(gi.offset_x as i64), 
-                    Value::Integer(gi.offset_y as i64), 
-                    Value::Integer(gi.width as i64), 
-                    Value::Integer(gi.height as i64), 
-                    Value::Integer(gi.tile_east as i64), 
-                    Value::Integer(gi.tile_south as i64), 
-                    Value::Integer(gi.unknown[0] as i64), 
-                    Value::Integer(gi.unknown[1] as i64), 
-                    Value::Integer(gi.unknown[2] as i64), 
-                    Value::Integer(gi.unknown[3] as i64), 
-                    Value::Integer(gi.unknown[4] as i64), 
-                    Value::Integer(gi.map as i64)
-                ]).unwrap();
-
-                let _ = statement.next().unwrap();
-            }
     }
 }
