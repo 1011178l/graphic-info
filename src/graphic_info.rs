eecea0ef2f231eae3e1e39c6f275c6f64207d8c4
Vincent Chi <song374561@chivincent.net>
2020-01-02 12:34:36 +0800

    feat: make graphic info can be serialized and deserialized.

--- src/graphic_info.rs
+++ src/graphic_info.rs
@@ -1,7 +1,8 @@
 use std::io::Cursor;
 use scroll::IOread;
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct GraphicInfo {
     pub id: u32,
     pub address: u32,
@@ -49,26 +50,66 @@ impl GraphicInfo {
     }
 }
 
-#[test]
-fn test_new() {
-    let bytes = [
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00,
-        0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00,
-        0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0xe7, 0x03, 0x00, 0x00
-    ];
-    let graphic_info = GraphicInfo::new(&bytes).unwrap();
+#[cfg(test)]
+mod test {
 
-    assert_eq!(graphic_info.id, 0);
-    assert_eq!(graphic_info.address, 0);
-    assert_eq!(graphic_info.length, 424);
-    assert_eq!(graphic_info.offset_x, -32);
-    assert_eq!(graphic_info.offset_y, -24);
-    assert_eq!(graphic_info.width, 64);
-    assert_eq!(graphic_info.height, 47);
-    assert_eq!(graphic_info.tile_east, 1);
-    assert_eq!(graphic_info.tile_south, 1);
-    assert_eq!(graphic_info.access, 1);
-    assert_eq!(graphic_info.unknown, [0, 0, 0, 0, 0]);
-    assert_eq!(graphic_info.map, 999);
+    use super::GraphicInfo;
+    use bincode;
+
+    #[test]
+    fn test_new() {
+        let bytes = [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00,
+            0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00,
+            0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0xe7, 0x03, 0x00, 0x00
+        ];
+        let graphic_info = GraphicInfo::new(&bytes).unwrap();
+    
+        assert_eq!(graphic_info.id, 0);
+        assert_eq!(graphic_info.address, 0);
+        assert_eq!(graphic_info.length, 424);
+        assert_eq!(graphic_info.offset_x, -32);
+        assert_eq!(graphic_info.offset_y, -24);
+        assert_eq!(graphic_info.width, 64);
+        assert_eq!(graphic_info.height, 47);
+        assert_eq!(graphic_info.tile_east, 1);
+        assert_eq!(graphic_info.tile_south, 1);
+        assert_eq!(graphic_info.access, 1);
+        assert_eq!(graphic_info.unknown, [0, 0, 0, 0, 0]);
+        assert_eq!(graphic_info.map, 999);
+    }
+
+    #[test]
+    fn test_serialize() {
+        let graphic_info = GraphicInfo {
+            id: 0, address: 0, length: 424, offset_x: -32, offset_y: -24, width: 64, height: 47, tile_east: 1, tile_south: 1, access: 1, unknown: [0, 0, 0, 0, 0], map: 999
+        };
+
+        let bytes = bincode::serialize(&graphic_info).unwrap();
+        let expect =  [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00,
+            0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00,
+            0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0xe7, 0x03, 0x00, 0x00
+        ];
+
+        assert!(bytes.iter().eq(expect.iter()));
+    }
+
+    #[test]
+    fn test_deserialize()
+    {
+        let bytes =  [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa8, 0x01, 0x00, 0x00,
+            0xe0, 0xff, 0xff, 0xff, 0xe8, 0xff, 0xff, 0xff, 0x40, 0x00, 0x00, 0x00,
+            0x2f, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0xe7, 0x03, 0x00, 0x00
+        ];
+        let graphic_info: GraphicInfo = bincode::deserialize(&bytes).unwrap();
+
+        assert_eq!(graphic_info, GraphicInfo {
+            id: 0, address: 0, length: 424, offset_x: -32, offset_y: -24, width: 64, height: 47, tile_east: 1, tile_south: 1, access: 1, unknown: [0, 0, 0, 0, 0], map: 999
+        });
+    }
 }
